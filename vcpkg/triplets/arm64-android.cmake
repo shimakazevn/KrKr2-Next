ab85da3b01695ff094c8d8c4d481e39e5174fae5
shimakazevn <shimakazevn@gmail.com>
2026-07-22 15:15:49 +0700

    Fix 7zip build on arm64-android: add -march=armv8-a+crc to C/CXX flags

--- vcpkg/triplets/arm64-android.cmake
+++ vcpkg/triplets/arm64-android.cmake
@@ -6,4 +6,7 @@ set(VCPKG_MAKE_BUILD_TRIPLET "--host=aarch64-linux-android")
 set(VCPKG_CMAKE_CONFIGURE_OPTIONS -DANDROID_ABI=arm64-v8a)
 set(VCPKG_CMAKE_SYSTEM_VERSION 24)
 
+set(VCPKG_C_FLAGS "-march=armv8-a+crc")
+set(VCPKG_CXX_FLAGS "-march=armv8-a+crc")
+
 include(${CMAKE_CURRENT_LIST_DIR}/android-dynamic-libs.cmake)
