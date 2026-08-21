598d85f29ac4ea00bcb5867810079556dffc201f
shimakazevn <shimakazevn@gmail.com>
2026-08-21 21:09:39 +0700

    fix(android): remove game mode config to resolve AAPT supportsBatteryMode error without needing compileSdk 36

--- apps/flutter_app/android/app/build.gradle.kts
+++ apps/flutter_app/android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "org.github.krkr2.flutter_app"
-    compileSdk = 36
+    compileSdk = flutter.compileSdkVersion
     ndkVersion = "29.0.14206865"
 
     compileOptions {
@@ -25,7 +25,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = 36
+        targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
 
